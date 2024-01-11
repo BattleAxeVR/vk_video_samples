3a3917b1e7e4e556c863aba0833d721f310016ba
BattleAxeVR <belakampis@gmail.com>
2024-01-11 03:01:48 -0500

    Update video_pixel_shader.frag.hlsl

--- vk_video_decoder/video_pixel_shader.frag.hlsl
+++ vk_video_decoder/video_pixel_shader.frag.hlsl
@@ -1,14 +1,8 @@
 [[vk::binding(0, 0)]]
-Texture2D textureColor;
+Texture2D textureColor : register(t0);
 
-//Texture2D textureY;// : register(t0);
-//Texture2D textureUV;// : register(t0);
-
-// slangc complains if here is no vk::binding decorator. but no matter what value I set, the output is red, so it's not using the immutable sampler, but some other default one
-// see : https://github.com/KhronosGroup/Vulkan-Docs/issues/985
-
-//[[vk::binding(1, 0)]]
-SamplerState samplerColor;// : register(s0);
+[[vk::binding(1, 0)]]
+SamplerState samplerColor : register(s0);
 
 struct VSOutput
 {
@@ -18,10 +12,5 @@ struct VSOutput
 float4 main(VSOutput input) : SV_TARGET
 {
 	float4 colour = textureColor.Sample(samplerColor, input.UV);
-
-	//float4 colour = textureColor.SampleLevel(samplerColor, input.UV, 0.0);
-	//float4 colour = textureY.Sample(samplerColor, input.UV);
-	//colour.gb = textureUV.Sample(samplerColor, input.UV).rg;
-	
 	return colour;
 }
