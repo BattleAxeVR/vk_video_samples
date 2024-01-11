32b8245c3ec1ef65bc91559a88ea9b99ee14d3de
BattleAxeVR <belakampis@gmail.com>
2024-01-11 01:20:07 -0500

    Reproed the bug using dxc

--- vk_video_decoder/compile_hlsl_video_shaders.bat
+++ vk_video_decoder/compile_hlsl_video_shaders.bat
@@ -7,5 +7,5 @@ REM glslcc.exe --frag=video_pixel_shader.frag --output=video_pixel_shader.frag.h
 REM dxc -Zi -E main -Od -T vs_6_6 -nologo -spirv -fspv-target-env=vulkan1.3 video_vertex_shader.vert.hlsl /E"main" /Fo"video_vertex_shader.spv" /nologo 
 REM dxc -Zi -E main -Od -T ps_6_6 -nologo -spirv -fspv-target-env=vulkan1.3 video_pixel_shader.frag.hlsl /E"main" /Fo"video_pixel_shader.spv" /nologo 
 
-slangc video_vertex_shader.vert.hlsl -entry main -stage vertex -o video_vertex_shader.spv
+REM slangc video_vertex_shader.vert.hlsl -entry main -stage vertex -o video_vertex_shader.spv
 slangc video_pixel_shader.frag.hlsl -entry main -stage fragment -o video_pixel_shader.spv
