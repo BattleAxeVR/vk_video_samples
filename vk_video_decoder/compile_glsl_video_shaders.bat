f9e334d475798e0129a3608b9bf77a1ab6dc3c32
BattleAxeVR <belakampis@gmail.com>
2024-01-10 23:55:23 -0500

    Load SPVs from file works with GLSL -> SPV batch file

---
+++ vk_video_decoder/compile_glsl_video_shaders.bat
@@ -0,0 +1,2 @@
+glslangValidator.exe video_vertex_shader.vert -o video_vertex_shader.spv --target-env vulkan1.3
+glslangValidator.exe video_pixel_shader.frag -o video_pixel_shader.spv --target-env vulkan1.3
