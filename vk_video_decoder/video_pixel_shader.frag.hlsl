8804f0c39c6e7759655ea2889459511a6932624e
BattleAxeVR <belakampis@gmail.com>
2024-01-11 01:17:32 -0500

    Update video_pixel_shader.frag.hlsl

--- vk_video_decoder/video_pixel_shader.frag.hlsl
+++ vk_video_decoder/video_pixel_shader.frag.hlsl
@@ -1,29 +1,13 @@
-Texture2D<float4> tex00[1] : register(t0);
-SamplerState _tex00_sampler[1] : register(s0);
+Texture2D textureColor : register(t0);
+SamplerState samplerColor : register(s0);
 
-static float4 oFrag;
-static float2 vTexCoord;
-
-struct SPIRV_Cross_Input
+struct VSOutput
 {
-    float2 vTexCoord : TEXCOORD0;
+[[vk::location(0)]] float2 UV : TEXCOORD0;
 };
 
-struct SPIRV_Cross_Output
+float4 main(VSOutput input) : SV_TARGET
 {
-    float4 oFrag : SV_Target0;
-};
-
-void frag_main()
-{
-    oFrag = tex00[0].Sample(_tex00_sampler[0], vTexCoord);
-}
-
-SPIRV_Cross_Output main(SPIRV_Cross_Input stage_input)
-{
-    vTexCoord = stage_input.vTexCoord;
-    frag_main();
-    SPIRV_Cross_Output stage_output;
-    stage_output.oFrag = oFrag;
-    return stage_output;
+	float4 colour = textureColor.SampleLevel(samplerColor, input.UV, 0.0);
+	return colour;
 }
