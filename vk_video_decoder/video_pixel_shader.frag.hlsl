6a296205872a6a5b44af5d726eab78b50c46bec7
BattleAxeVR <belakampis@gmail.com>
2024-01-11 19:22:40 -0500

    SampleGrad crashes during pipeline creation

--- vk_video_decoder/video_pixel_shader.frag.hlsl
+++ vk_video_decoder/video_pixel_shader.frag.hlsl
@@ -7,6 +7,9 @@ struct VSOutput
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	float4 colour = tex.Sample(input.UV);
+	//float4 colour = tex.Sample(input.UV);
+	float4 colour = tex.SampleLevel(input.UV, 0.0);
+	//float4 colour = tex.SampleGrad(input.UV, float2(0,0), float2(0,0));//, 0, 0.5f);
+	
 	return colour;
 }
